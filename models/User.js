f66e0d9dd074e89405982871b6471dd5b4e9027e
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-03-09 19:32:51 -0600

    added User model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,34 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+// creates User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
