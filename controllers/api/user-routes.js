44c3006e6187025542564c7e88b01c1de5e91fff
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-03-13 22:09:33 -0700

    initial user-routes sign in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,72 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection');
+const { User } = require('../../models');
 
+// create a new user
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            first_name: req.body.first_name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        // set up session
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// login
+router.post('/login', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        });
+
+        if (!dbUserData) {
+            res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+            return;
+        }
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+            return;
+        }
+
+        // Once the user successfully logs in, set up the sessions variable 'loggedIn'
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// logout and destroy session
+router.post('logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(4040).end();
+    }
+});
+
+module.exports - router;
 
 
 
--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // creates User model
-class User extends Model {}
+class User extends Model { }
 
 // define table columns and configuration
 User.init(
@@ -20,15 +20,34 @@ User.init(
             validate: {
                 isEmail: true
             }
-        }
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlpha: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                leng: [6, 12]
+            },
+        },
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: 'user'
-    }
-);
+    });
 
 module.exports = User;
