ea68b5c4da3d0f353df908fe19fd4a7f8464296b
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-03-20 20:14:10 -0700

    added css

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session')
+const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -17,7 +18,7 @@ const sess = {
     })
 };
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
